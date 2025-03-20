bbcc73a3489692e4817715706dfde6424b503756
EveLine15 <nnasta788@gmail.com>
2025-03-20 18:49:14 +0300

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,5 @@
 import "./Footer.scss"
 import Contacts from "./Contacts/Contacts"
-import BigColumn from "./Columns/BigColumn"
-import SmallColumn from "./Columns/SmallColumn"
 import Column from "./Columns/Column"
 import footerColumn from "../../data/footerColumn.json"
 
@@ -19,9 +17,6 @@ export default function Footer(){
                             
                         })
                     }
-                    {/* <BigColumn label="Product" text1="Download" text2="Pricing" text3="Locations" text4="Server" text5="Countries" text6="Blog"/>
-                    <BigColumn label="Engage" text1="LaslesVPN?" text2="FAQ" text3="Tutorials" text4="About us" text5="Privacy policy" text6="Terms of service"/>
-                    <SmallColumn label="Earn money" text1="Become partner" text2="Affilite"/> */}
                 </div>
             </div>
         </div>
